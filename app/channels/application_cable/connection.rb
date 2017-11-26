e411ceae00a7d1c31e6e22252268a19bbd040470
Edu Saborit <edu@barkibu.com>
2017-11-26 19:48:20 +0100

    Setup ActionCable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_player
+
+    def connect
+      current_player = Player.find_by(id: cookies[:player_id])
+      reject_unauthorized_connection if current_player.blank?
+    end
   end
 end
